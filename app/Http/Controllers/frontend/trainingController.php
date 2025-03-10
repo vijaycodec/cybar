067de7542be8877c41206ebe36f14299c0a8898f
vijaycodec <vijaycodec@gmail.com>
2025-03-10 10:44:57 +0530

    add google analytics

--- app/Http/Controllers/frontend/trainingController.php
+++ app/Http/Controllers/frontend/trainingController.php
@@ -21,22 +21,27 @@ class trainingController extends Controller
         ->where('page_category', $page_id)
         ->get();
 
-        $seoData = Seo::where('page_name', $page_name)
+        $seoDetails = Seo::where('page_name', $page_name)
         ->where('category_name', $category)
         ->where('sub_category_name', $sub_category)
         ->first();
 // dd( $seoData );
     // Define default values if any field is NULL
         $seoData = [
-            'page_name'       => $seoData->page_name ?? 'Training Page',
-            'category_name'   => $seoData->category_name ?? 'General Training Category',
-            'sub_category_name' => $seoData->sub_category_name ?? 'General Training Subcategory',
-            'template_name'   => $seoData->template_name ?? 'default-template',
-            'seo_title'       => $seoData->seo_title ?? 'Default Service Title',
-            'seo_description' => $seoData->seo_description ?? 'Default Training Description',
-            'seo_keywords'    => $seoData->seo_keywords ?? 'default,Training keywords',
-            'google_analytics' => $seoData->google_analytics ?? 'Training google analytics',
+            'page_name'       => $seoDetails->page_name ?? 'Training Page',
+            'category_name'   => $seoDetails->category_name ?? 'General Training Category',
+            'sub_category_name' => $seoDetails->sub_category_name ?? 'General Training Subcategory',
+            'template_name'   => $seoDetails->template_name ?? 'default-template',
+            'seo_title'       => $seoDetails->seo_title ?? 'Default Service Title',
+            'seo_description' => $seoDetails->seo_description ?? 'Default Training Description',
+            'seo_keywords'    => $seoDetails->seo_keywords ?? 'default,Training keywords',
         ];
+
+        if (!empty($seoDetails->google_analytics)) {
+
+            $seoData['google_analytics'] = $seoDetails->google_analytics;
+        }
+
         return view('frontend.training', compact('categories','trainings','page_id', 'seoData'));
     }
 }
