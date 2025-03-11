6459f813c2bb395f02bb8f4a065d6d33197abbdb
vijaycodec <vijaycodec@gmail.com>
2025-03-11 12:28:02 +0530

    add seo index blade backend

--- resources/views/backend/seo-details/index.blade.php
+++ resources/views/backend/seo-details/index.blade.php
@@ -4,14 +4,14 @@
     <div class="main-content-inner">
         <div class="main-content-wrap">
             <div class="flex items-center flex-wrap justify-between gap20 mb-27">
-                <h3>All Resources</h3>
+                <h3>All SEO</h3>
                 <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
                     <li><a href="{{ route('admin.dashboard') }}">
-                            <div class="text-tiny">Dashboard</div>
+                            <div class="text-tiny">SEO</div>
                         </a></li>
                     <li><i class="icon-chevron-right"></i></li>
                     <li>
-                        <div class="text-tiny">All Resources</div>
+                        <div class="text-tiny">All SEO</div>
                     </li>
                 </ul>
             </div>
@@ -29,67 +29,53 @@
                             </div>
                         </form>
                     </div>
-                    <a class="tf-button style-1 w208" href=" {{ route('resources.create') }}">
+                    <a class="tf-button style-1 w208" href=" {{ route('seo-details.create') }}">
                         <i class="icon-plus"></i>Add New</a>
                 </div>
 
                 <div class="table-responsive">
-                    <table id="resourcesTable" class="table table-striped table-bordered " style="table-layout: auto;">
+                    <table id="blogsTable" class="table table-striped table-bordered " style="table-layout: auto;">
                         <thead>
                             <tr>
-                                <th>#</th>
-                                <th>Category Name</th>
-                                <th>Image</th>
-                                <th>Short Title</th>
-                                <th>Slug</th>
-                                <th>Description</th>
-                                <th>Action</th>
+                                <th>ID</th>
+                                <th>Page Category</th>
+                                <th>Category</th>
+                                <th>Sub Category</th>
+                                <th>Template Name</th>
+                                <th>SEO Title</th>
+                                <th>SEO Description</th>
+                                <th>SEO Keywords</th>
+                                <th>Google Analytics</th>
+                                <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @if ($resources->count() > 0)
-                                @foreach ($resources as $resource)
+                            @if ($seodetails->count() > 0)
+                                @foreach ($seodetails as $seodetail)
                                     <tr>
-                                        <td>{{ $resource->id }}</td>
-                                        <td>{{ $resource->category->name ?? 'N/A' }}</td> <!-- Using the relationship -->
-                                        <td>
-                                            @if ($resource->images)
-                                                <img src="{{ asset('uploads/backend/resources/' . $resource->images) }}"
-                                                    alt="Resource Image" width="100">
-                                            @else
-                                                <span>No image available</span>
-                                            @endif
-                                        </td>
-                                        <td style="max-width: 400px;"> <!-- Increase width here -->
-                                            <!-- Short description (up to 10 words) -->
-                                            <span>{{ $resource->short_desc }}</span>
-                                        </td>
-                                        <td>{{ $resource->slug ?? 'N/A' }}</td> 
-                                        <td style="max-width: 400px;"> <!-- Increase width here -->
-                                            <!-- Short description (up to 10 words) -->
-                                            <span class="description-short">
-                                                {!! $resource->short_description ?? mb_strimwidth(html_entity_decode($resource->description), 0, 19, '...') !!}
-                                            </span>
-                                            <!-- Read More button for longer descriptions -->
-                                            @if (str_word_count(strip_tags($resource->description)) > 19)
-                                                <button class="btn btn-link read-more"
-                                                    data-id="{{ $resource->category->name }}"
-                                                    data-description="{{ html_entity_decode($resource->description) }}">Read
-                                                    More</button>
-                                            @endif
-                                        </td>
+                                        <td>{{ $seodetail->id }}</td>
+                                        <td>{{ $seodetail->seopageCategory->page_name ?? 'N/A' }}</td>
+                                        <td>{{ $seodetail->seocategory->name ?? 'N/A' }}</td>
+                                        <td>{{ $seodetail->seosubCategory->sub_category ?? 'N/A' }}</td>
+                                        <td>{{ $seodetail->template_name }}</td>
+                                        <td>{{ $seodetail->seo_title }}</td>
+                                        <td>{{ $seodetail->seo_description }}</td>
+                                        <td>{{ $seodetail->seo_keywords }}</td>
+                                        <td>{{ $seodetail->google_analytics }}</td>  
+                                       
+                                       
                                         <td>
                                             <div class="list-icon-function">
-                                                <button type="button" class="show" data-id="{{ $resource->id }}">
+                                                <button type="button" class="show" data-id="{{ $seodetail->id }}">
                                                     <div class="item eye">
                                                         <i class="icon-eye"></i>
                                                     </div>
                                                 </button>
-                                                <a href="{{ route('resources.edit', $resource->id) }}">
+                                                <a href="{{ route('seo-details.edit', $seodetail->id) }}">
                                                     <div class="item edit">
                                                         <i class="icon-edit-3"></i>
                                                     </div>
-                                                </a> <button type="button" class="delete" data-id="{{ $resource->id }}">
+                                                </a> <button type="button" class="delete" data-id="{{ $seodetail->id }}">
                                                 <div class="item text-danger">
                                                     <i class="icon-trash-2"></i>
                                                 </div>
@@ -100,7 +86,7 @@
                                 @endforeach
                             @else
                                 <tr>
-                                    <td colspan="6" class="text-center">No data found</td>
+                                    <td colspan="10" class="text-center">No data found</td>
                                 </tr>
                             @endif
                         </tbody>
@@ -108,7 +94,7 @@
 
                     <!-- Pagination links -->
                     <div>
-                        {!! $resources->withQueryString()->links('pagination::bootstrap-5') !!}
+                        {!! $seodetails->withQueryString()->links('pagination::bootstrap-5') !!}
                     </div>
                 </div>
             </div>
@@ -116,22 +102,22 @@
     </div>
 
     <!-- Modal Structure for Read more button-->
-    <div class="modal fade" id="resourceModal" tabindex="-1" aria-labelledby="resourceModalLabel" aria-hidden="true">
+    <div class="modal fade" id="seodetailModal" tabindex="-1" aria-labelledby="seodetailModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content rounded-3 shadow-lg">
                 <div class="modal-header bg-primary text-white">
-                    <h5 class="modal-title text-white" id="resourceModalLabel">Resource Details</h5>
+                    <h5 class="modal-title text-white" id="seodetailModalLabel">seodetail Details</h5>
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
                         aria-label="Close"></button>
                 </div>
                 <div class="modal-body p-4" style="max-height: 70vh; overflow-y: auto;">
                     <div class="row mb-10">
-                        <div class="col-md-4 fw-bold mb-5">Resource Name:</div>
-                        <div class="col-md-8" id="modal-resource-name"></div>
+                        <div class="col-md-4 fw-bold mb-5">seodetail Name:</div>
+                        <div class="col-md-8" id="modal-seodetail-user_name"></div>
                     </div>
                     <div class="row mb-3">
                         <div class="col-md-4 fw-bold">Full Description:</div>
-                        <div class="col-md-8" id="modal-resource-description"></div>
+                        <div class="col-md-8" id="modal-seodetail-description"></div>
                     </div>
                 </div>
                 <div class="modal-footer bg-light">
@@ -141,18 +127,18 @@
         </div>
     </div>
 
-    <div class="modal fade" id="resourceModalShow" tabindex="-1" aria-labelledby="resourceModalLabel" aria-hidden="true">
+    <div class="modal fade" id="seodetailModalShow" tabindex="-1" aria-labelledby="seodetailModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content rounded-3 shadow-lg">
                 <div class="modal-header bg-primary text-white">
-                    <h5 class="modal-title text-white" id="resourceModalLabel">Resource Details</h5>
+                    <h5 class="modal-title text-white" id="seodetailModalLabel">seodetail Details</h5>
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
                         aria-label="Close"></button>
                 </div>
                 <div class="modal-body p-4" style="max-height: 70vh; overflow-y: auto;">
                     <div class="row mb-10">
-                        <div class="col-md-4 fw-bold mb-5">Resource Name:</div>
-                        <div class="col-md-8" id="resource_name"></div>
+                        <div class="col-md-4 fw-bold mb-5">seodetail Name:</div>
+                        <div class="col-md-8" id="seodetail_name"></div>
                     </div>
                     <div class="row mb-3">
                         <div class="col-md-4 fw-bold">short Desc:</div>
@@ -164,7 +150,7 @@
                     </div>
                     <div class="row mb-3">
                         <div class="col-md-4 fw-bold">Created At:</div>
-                        <div class="col-md-8" id="resources-created-at"></div>
+                        <div class="col-md-8" id="seodetails-created-at"></div>
                     </div>
                 </div>
                 <div class="modal-footer bg-light">
@@ -178,88 +164,88 @@
 @endsection
 
 @push('scripts')
-    <script>
-        $(document).ready(function() {
-            // Handle Read More button click
-            $(document).on('click', '.read-more', function() {
-                var resourceId = $(this).data('id');
-                var fullDescription = $(this).data('description');
+    
+<script>
+    $(document).ready(function() {
+        // Handle Read More button click
+        $(document).on('click', '.read-more', function() {
+            var categoryName = $(this).data('category'); // Get category name
+            var fullDescription = $(this).data('description');
 
-                // Set the full description in the modal
-                $('#modal-resource-name').text(resourceId); // to do category name instead of resource Id 
-                $('#modal-resource-description').html(fullDescription);
+            // Set the category name in the modal instead of seodetailId
+            $('#modal-blog-name').text(categoryName);
+            $('#modal-seodetail-description').html(fullDescription);
 
-                // Show the modal
-                $('#resourceModal').modal('show');
-            });
+            // Show the modal
+            $('#seodetailModal').modal('show');
+        });
 
-
-            //script for Delete
-            $(document).on('click', '.delete', function() {
-                var resourceId = $(this).data('id'); // Get ID from data attribute
-                Swal.fire({
-                    title: 'Are you sure?',
-                    text: "You won't be able to revert this!",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Yes, delete it!'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        // Perform AJAX request to delete
-                        $.ajax({
-                            url: "{{ route('resources.destroy', ':id') }}".replace(':id',
-                                resourceId),
-                            method: "DELETE",
-                            data: {
-                                _token: "{{ csrf_token() }}" // CSRF Token
-                            },
-                            success: function(response) {
-                                Swal.fire(
-                                    'Deleted!',
-                                    response.message,
-                                    'success'
-                                ).then(() => {
-                                    location
-                                        .reload(); // Reload the page or update the DOM
-                                });
-                            },
-                            error: function(xhr) {
-                                Swal.fire(
-                                    'Error!',
-                                    'Unable to delete. Please try again later.',
-                                    'error'
-                                );
-                            }
-                        });
-                    }
-                });
+        // Script for Delete
+        $(document).on('click', '.delete', function() {
+            var seodetailId = $(this).data('id'); // Get ID from data attribute
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Perform AJAX request to delete
+                    $.ajax({
+                        url: "{{ route('seo-details.destroy', ':id') }}".replace(':id', seodetailId),
+                        method: "POST", // Use POST instead of DELETE (Laravel handles it with _method)
+                        data: {
+                            _method: "DELETE", // Laravel's way to handle DELETE requests
+                            _token: "{{ csrf_token() }}" // CSRF Token
+                        },
+                        success: function(response) {
+                            Swal.fire(
+                                'Deleted!',
+                                response.message,
+                                'success'
+                            ).then(() => {
+                                location.reload(); // Reload the page or update the DOM
+                            });
+                        },
+                        error: function(xhr) {
+                            Swal.fire(
+                                'Error!',
+                                'Unable to delete. Please try again later.',
+                                'error'
+                            );
+                        }
+                    });
+                }
             });
         });
-    </script>
+    });
+</script>
+
 
     {{-- script for update  --}}
     <script>
         $(document).ready(function() {
             // Show permission details in modal
             $(document).on('click', '.show', function() {
-                var resourcesId = $(this).data('id');
+                var seodetailsId = $(this).data('id');
                 $.ajax({
-                    url: "{{ route('resources.show', ':id') }}".replace(':id', resourcesId),
+                    url: "{{ route('seo-details.show', ':id') }}".replace(':id', seodetailsId),
                     method: "GET",
-                    success: function(response) {
+                    success: function(seodetail) {
                         // Correctly access properties with a hyphen
-                        $('#resource_name').text(response
-                            .resource_name); // Use default value if undefined
+                        $('#seodetail_name').text(seodetail
+                            .seodetail_name); // Use default value if undefined
                         $('#short_desc').text(response.short_desc); // Handle null or undefined
                         $('#description').html(response.description);
-                        $('#resources-created-at').text(response.created_at);
-                        $('#resourceModalShow').modal('show');
+                        $('#seodetails-created-at').text(response.created_at);
+                        $('#seodetailModalShow').modal('show');
 
                     },
                     error: function() {
-                        Swal.fire('Error!', 'Unable to fetch resources details.', 'error');
+                        Swal.fire('Error!', 'Unable to fetch seodetails details.', 'error');
                     }
                 });
             });
