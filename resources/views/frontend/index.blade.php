265d0d83752d58f6c5b7d7fc33ba94c85f462907
dhruvboolchandani <dhruvboolchandani@gmail.com>
2025-03-22 12:15:40 +0530

    fixed scroll position on homepage

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -5968,7 +5968,8 @@
         if (!isMobile()) {
             function scrollToFirstElement(className, offset = 100) {
                 document.querySelectorAll(`.${className}`).forEach(element => {
-                    element.addEventListener("click", function () {
+                    element.addEventListener("click", function (e) {
+                        e.preventDefault();
                         setTimeout(() => {
                             const firstElement = document.querySelector(`.${className}`);
                             if (firstElement) {
@@ -5984,10 +5985,10 @@
         }
 
         // Example usage:
-        scrollToFirstElement("scr-tabs", 180);
-        scrollToFirstElement("scr-tabs1", 180);
-        scrollToFirstElement("scr-tabs3", 200);
-        scrollToFirstElement("industry-slider", 150);
+        scrollToFirstElement("scr-tabs", 80);
+        scrollToFirstElement("scr-tabs1", 80);
+        scrollToFirstElement("scr-tabs3", 80);
+        scrollToFirstElement("industry-slider", 50);
     </script>
     @include('frontend.layouts.right-menu-js')
 @endpush
